89545cdc3a4c7f7c3f36d3857be7242851c3efad
Worldghoul <alex.j.cook@gmail.com>
2015-11-04 19:44:37 -0800

    Update Intro.java

--- Intro.java
+++ Intro.java
@@ -26,6 +26,7 @@ public class Intro {
             add("Clio");
             add("Toucan");
             add("Clare");
+            add("Burger Meister Meister Burger")
         }};
 
         // What this does is take each person and print to your terminal the
